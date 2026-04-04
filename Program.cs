2c3ba3d46bdd8a1f3eb936ebf0092df772dc8e81
NeoPenaflor <neopenaflor8@gmail.com>
2026-04-04 19:18:37 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
+using System;
+using System.Collections.Generic;
 namespace LoanInterestNotif
-//(Nilipat sa ibang file yung mga running)
 {
     internal class Program
     {
@@ -34,6 +35,7 @@ namespace LoanInterestNotif
             }
 
             DisplayLogs();
+            LoanForm();
         }
 
         static bool LoginOption()
@@ -109,5 +111,78 @@ namespace LoanInterestNotif
             foreach (var log in accessLogs)
             {
                 Console.WriteLine(log);
-            
-                
+            }
+        }   
+   
+static void LoanForm()
+        {
+Console.WriteLine("Input your current job");
+       string job = Console.ReadLine();
+
+Console.WriteLine("Input your current salary");
+       string salary = Console.ReadLine();
+
+Console.WriteLine("Input your current company");
+        string company = Console.ReadLine();
+
+Console.WriteLine("Select how long do you want to loan the money");
+Console.WriteLine("1. 3 months");
+Console.WriteLine("2. 6 months");
+Console.WriteLine("3. 9 months");
+Console.WriteLine("4. 12 months");
+
+int choice =    Convert.ToInt32(Console.ReadLine());
+int loanMonths = 0;
+
+switch (choice)
+          {
+case 1: 
+loanMonths = 3; 
+break;
+
+case 2: 
+loanMonths = 6; 
+break;
+
+case 3: 
+loanMonths = 9; 
+break;
+
+case 4: 
+loanMonths = 12;
+break;
+
+default:
+Console.WriteLine("Invalid choice");
+return;
+            }
+
+double interestRate;
+switch (loanMonths)
+         {
+case 3: 
+interestRate = 0.05; 
+break;
+
+case 6: 
+interestRate = 0.10; 
+break;
+
+case 9: 
+interestRate = 0.15;
+break;
+
+case 12: 
+interestRate = 0.20; 
+break;
+
+default: 
+interestRate = 0; 
+break;
+        }
+
+Console.WriteLine($"Loan term: {loanMonths} months");
+Console.WriteLine($"Interest rate: {interestRate * 100}%");
+        }
+    }
+}
