a67d0bacf038c9388437ca3a5c31b5d81d54be30
NeoPenaflor <neopenaflor8@gmail.com>
2026-04-13 14:14:38 +0800

    Update SystemAppService.cs

--- NotificationAppService/SystemAppService.cs
+++ NotificationAppService/SystemAppService.cs
@@ -1,7 +1,23 @@
+using System;
+using System.Collections.Generic;
+using LoanNotificationDataService;
+using LoanDataModel;
+
 ﻿namespace NotificationAppService
 {
     public class SystemAppService
-    {
+    { 
+private readonly MediatorDataService mediator;
 
-    }
+public SystemAppService(MediatorDataService mediatorService)
+        { mediator = mediatorService;}
+public void Create(SystemDataModel data)
+        {mediator.Create(data);}
+public List<SystemDataModel> GetAll()
+        {return mediator.View();}
+public void Update(SystemDataModel data)
+        {mediator.Update(data);}
+public void Delete(Guid id)
+        {mediator.Delete(id);}
+     }
 }
