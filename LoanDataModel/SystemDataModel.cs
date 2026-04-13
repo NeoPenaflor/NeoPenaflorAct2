7a160146c790e82c6e657074865ca427bfd5b68e
NeoPenaflor <neopenaflor8@gmail.com>
2026-04-13 08:09:33 +0800

    Update SystemDataModel.cs

--- LoanDataModel/SystemDataModel.cs
+++ LoanDataModel/SystemDataModel.cs
@@ -1,9 +1,15 @@
+using System;
 ﻿namespace LoanDataModel
+
 {
     public class SystemDataModel
     {
-        public string Name { get; set; }
+    public string Name         { get;set; }
+    public Guid Id             { get;set; }
+    public string Job          { get;set; }
+    public double Salary       { get;set; }
+    public string Company      { get;set; }
+    public int LoanMonths      { get;set; }
+    public double InterestRate { get;set; }
     }
 }
-//gagawa variable
-//get set 
