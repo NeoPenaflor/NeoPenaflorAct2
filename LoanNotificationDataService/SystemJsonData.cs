7108e46e10790c5f2661be80be71ffba2393be6d
NeoPenaflor <neopenaflor8@gmail.com>
2026-04-17 23:49:25 +0800

    Update SystemJsonData.cs

--- LoanNotificationDataService/SystemJsonData.cs
+++ LoanNotificationDataService/SystemJsonData.cs
@@ -10,69 +10,64 @@ using System.IO;
 
 namespace LoanNotificationDataService
 {
-    public class SystemJsonData : InterfaceLoanDataService
+    public class SystemJsonData :     InterfaceLoanDataService
     {
     private List<SystemDataModel> Notiflist = new List<SystemDataModel>();
     private string _jsonFileName;
     public SystemJsonData()
-        {
-    _jsonFileName = $"{AppDomain.CurrentDomain.BaseDirectory}/LoanSystemJSONDataFile.json";
+    { _jsonFileName = $"{AppDomain.CurrentDomain.BaseDirectory}/LoanSystemJSONDataFile.json";
 PopulateJsonFile();
+         }
 
-    private void PopulateJsonFile()
-           {
+private void SaveDataToJsonFile()
+{
+    string jsonData = JsonSerializer.Serialize(Notiflist, new JsonSerializerOptions
+    {
+    WriteIndented = true
+    });
+
+File.WriteAllText(_jsonFileName, jsonData);
+}
+
+
+private void PopulateJsonFile()
+{
     if (!File.Exists(_jsonFileName))
     {
-    File.WriteAllText(_jsonFileName, "[]");      
-}
-    RetrieveDataFromJsonFile();
-   if (Notiflist.Count <= 0)
-        {
-Notiflist.Add(new SystemDataModel { Name = "Neo", Id = Guid.NewGuid(), Job = "Owner", Salary = 1000,Company = "PUP", LoanMonths = 12, InterestRate = 0.5,LoanAmount= 10000, TotalPayment= 15000 });
+File.WriteAllText(_jsonFileName, "[]");
+    }
 
-         SaveDataToJsonFile();
-            }
-}
-        private void SaveDataToJsonFile()
-        {
-            using (var outputStream = File.Create(_jsonFileName))
-            {     
-        JsonSerializer.Serialize(
-new Utf8JsonWriter(outputStream, new JsonWriterOptions
-                    { SkipValidation = true, Indented = true }), 
-        Notiflist
-);
-}
-
-private void RetrieveDataFromJsonFile()
- {
-using (var jsonFileReader = File.OpenText(_jsonFileName))
-            {
-                Notiflist = JsonSerializer.Deserialize<List<SystemDataModel>>
-                    (jsonFileReader.ReadToEnd(), new JsonSerializerOptions
-                    { PropertyNameCaseInsensitive = true })
-                    .ToList();
-            }
-        }
-  
-public void Delete(Guid Id)
-{
     RetrieveDataFromJsonFile();
 
-    var existing = Notiflist.FirstOrDefault(x => x.Id == Id);
-    if (existing != null)
+    if (Notiflist.Count <= 0)
     {
-        Notiflist.Remove(existing);
-        SaveDataToJsonFile();
+    Notiflist.Add(new SystemDataModel {Name = "Neo", Id = Guid.NewGuid(), Job = "Owner", Salary = 1000, Company = "PUP", LoanMonths = 12, InterestRate = 0.5, LoanAmount = 10000, TotalPayment = 15000
+    });
+    SaveDataToJsonFile();
     }
 }
+           
+private void RetrieveDataFromJsonFile()
+{ 
+     string jsonData =          
+     File.ReadAllText(_jsonFileName);
 
-
-//
-public void Update(SystemDataModel loanDS)
+     Notiflist = JsonSerializer.Deserialize<List<SystemDataModel>>(jsonData,
+        new JsonSerializerOptions
+        {
+PropertyNameCaseInsensitive = true
+}) ?? 
+     new List<SystemDataModel>();
+}
+public void Create(SystemDataModel loanDS)
 {
     RetrieveDataFromJsonFile();
-
+    Notiflist.Add(loanDS);
+    SaveDataToJsonFile();
+}
+public void Update(SystemDataModel    loanDS)
+{
+    RetrieveDataFromJsonFile();
     var existing = Notiflist.FirstOrDefault(x => x.Id == loanDS.Id);
 
     if (existing != null)
@@ -85,14 +80,25 @@ existing.LoanMonths = loanDS.LoanMonths;
 existing.InterestRate = loanDS.InterestRate;
 existing.LoanAmount = loanDS.LoanAmount;
 existing.TotalPayment = loanDS.TotalPayment;
-        SaveDataToJsonFile();
+ 
+    SaveDataToJsonFile();
+     }
+}
+    public void Delete(Guid Id)
+{
+    RetrieveDataFromJsonFile();
+    var existing = Notiflist.FirstOrDefault(x => x.Id == Id);
+    if (existing != null)
+    {
+    Notiflist.Remove(existing);
+    SaveDataToJsonFile();
     }
 }
 public List<SystemDataModel> View()
-{
+    {
     RetrieveDataFromJsonFile();
     return Notiflist;
-}      
+    }      
             
         }
     }
