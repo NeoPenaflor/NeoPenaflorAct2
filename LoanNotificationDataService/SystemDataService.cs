9838cf2831b42eebf4ae1ce414210c185be1616b
NeoPenaflor <neopenaflor8@gmail.com>
2026-04-17 16:39:06 +0800

    Update SystemDataService.cs

---
+++ LoanNotificationDataService/SystemDataService.cs
@@ -0,0 +1,27 @@
+using LoanDataModel;
+namespace LoanNotificationDataService
+{
+    public class SystemDataService : InterfaceLoanDataService
+
+    {
+        public void Create(SystemDataModel loanDS)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(SystemDataModel loanDS)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SystemDataModel> View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
