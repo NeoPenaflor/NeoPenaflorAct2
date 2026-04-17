f4c28112ff6cfd19b74a2cb8d6ae2ec48d28a776
NeoPenaflor <neopenaflor8@gmail.com>
2026-04-17 09:01:18 +0800

    Update SystemDataModel.cs

--- LoanDataModel/SystemDataModel.cs
+++ LoanDataModel/SystemDataModel.cs
@@ -7,11 +7,13 @@ using System;
     public string Name         { get;set; }
     public Guid Id             { get;set; }
     public string Job          { get;set; }
-    public double Salary       { get;set; }
+    public int Salary       
+{ get;set; }
     public string Company      { get;set; }
     public int LoanMonths      { get;set; }
     public double InterestRate { get;set; }
     public double LoanAmount { get; set; }
+    public double TotalPayment { get; set; }
     }
 }
 
